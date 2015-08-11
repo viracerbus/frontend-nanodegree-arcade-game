a4f794c2dc4996eb1fa0adb8c58ef85839605995
kevin.dexter <kevin.dexter@comcast.net>
2015-08-11 17:11:53 -0500

    Touch-ups
    Correcting a few mistakes

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ var Enemy = function() {
     this.movement = Math.floor(Math.random() * this.baseSpeed * 50 + this.baseSpeed * 50);
     this.y = Math.floor(Math.random() * 3);
     this.x = Math.floor(Math.random() * (-404) - 101);
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -26,7 +26,7 @@ Enemy.prototype.update = function(dt) {
 //  if (this.collide()) {
 //      Nothing for now. Maybe something in the future
 //  }
-}
+};
 
 // Collision detection for enemies
 Enemy.prototype.collide = function() {
@@ -34,19 +34,19 @@ Enemy.prototype.collide = function() {
         return true;
     }
     return false;
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y * 83 + 50);
-}
+};
 
 // Returns an enemy to the starting position
 Enemy.prototype.reset = function() {
     this.movement = Math.floor(Math.random() * this.baseSpeed * 50 + this.baseSpeed * 50);
     this.y = Math.floor(Math.random() * 3);
     this.x = Math.floor(Math.random() * (-404) - 101);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -58,7 +58,7 @@ var Player = function () {
     this.listOfChar = ['images/char-horn-girl.png', 'images/char-cat-girl.png', 'images/char-boy.png', 'images/char-pink-girl.png', 'images/char-princess-girl.png'];
     this.sprite = 'images/char-boy.png';
     this.score = 0;
-}
+};
 
 // Update method detects collisions with ememies
 Player.prototype.update = function () {
@@ -68,7 +68,7 @@ Player.prototype.update = function () {
             this.reset();
         }
     }
-}
+};
 
 // Draws player on screen
 Player.prototype.render = function() {
@@ -80,7 +80,7 @@ Player.prototype.render = function() {
     else {
         this.displaySelection();
     }
-}
+};
 
 // Renders the options for character avatar and difficulty setting.
 Player.prototype.displaySelection = function() {
@@ -90,7 +90,8 @@ Player.prototype.displaySelection = function() {
         ctx.font = '20px Verdana';
         ctx.fillText('Select with space...', 150, 200);
         ctx.drawImage(Resources.get('images/Selector.png'), this.x * 101, 4 * 83 + 50);
-        for (var option in this.listOfChar) {
+        var length = this.listOfChar.length;
+        for (var option = 0; option < length; option++) {
             ctx.drawImage(Resources.get(this.listOfChar[option]), option * 101, 382);
         }
     }
@@ -106,7 +107,7 @@ Player.prototype.displaySelection = function() {
             ctx.fillText(i + 1, i * 101 + 35, 425);
         }
     }
-}
+};
 
 // Reads in player input and behaves accordingly (moving player around screen or selecting option)
 Player.prototype.handleInput = function(input) {
@@ -132,20 +133,20 @@ Player.prototype.handleInput = function(input) {
         spawnEnemies();
         spawnGems();
     }
-}
+};
 
 // Increases the players score depending on Gems collected upon reaching the river
 Player.prototype.scorePoint = function () {
-    bonusPoint = true;
-    for (var gem in allGems) {
-        bonusPoint = (bonusPoint && !allGems[gem].visible);
-    }
+    var bonusPoint = true;
+    allGems.forEach(function(gem) {
+        bonusPoint = (bonusPoint && !gem.visible);
+    });
     if (bonusPoint === true) {
         this.score += 2;
-    } 
+    }
     this.score ++;
     this.reset();
-}
+};
 
 // Returns the player to home position and respawns gems
 Player.prototype.reset = function() {
@@ -154,27 +155,22 @@ Player.prototype.reset = function() {
     allGems.forEach(function(gem) {
         gem.reset();
     });
-}
+};
 
 // Spawns the enemies on the map depending on the difficulty
 var spawnEnemies = function () {
-    allEnemies.push(new Enemy());
-    allEnemies.push(new Enemy());
-    allEnemies.push(new Enemy());
-    if (difficulty > 1) {
+    limit = Math.min(5, difficulty + 2);
+    for (var i = 0; i < limit; i ++) {
         allEnemies.push(new Enemy());
-        if (difficulty > 2) {
-            allEnemies.push(new Enemy());
-        }
     }
-}
+};
 
 // Creates the Gems for the first time
 var spawnGems = function () {
     allGems.push(new GemBlue());
     allGems.push(new GemGreen());
     allGems.push(new GemOrange());
-}
+};
 
 // Gem items for added fun to the game
 var Gem = function (img) {
@@ -182,7 +178,7 @@ var Gem = function (img) {
     this.y = Math.floor(Math.random() * 3);
     this.visible = true;
     this.sprite = img;
-}
+};
 
 // Draws the gems on the map
 Gem.prototype.render = function() {
@@ -196,7 +192,7 @@ Gem.prototype.update = function() {
     if (this.collide()) {
         this.visible = false;
     }
-}
+};
 
 // Collision detection for the gem
 Gem.prototype.collide = function() {
@@ -204,29 +200,29 @@ Gem.prototype.collide = function() {
         return true;
     }
     return false;
-}
+};
 
 // Respawns the gem to a random spot behind enemy lines
 Gem.prototype.reset = function() {
     this.x = Math.floor(Math.random() * 5);
     this.y = Math.floor(Math.random() * 3);
     this.visible = true;
-}
+};
 
 // Subclasses for the Blue, Green and Orange gems (created as subclasses in case of future edits to Gem options)
 var GemBlue = function() {
     Gem.call(this, 'images/Gem Blue.png');
-}
+};
 GemBlue.prototype = Object.create(Gem.prototype);
 
 var GemGreen = function() {
     Gem.call(this, 'images/Gem Green.png');
-}
+};
 GemGreen.prototype = Object.create(Gem.prototype);
 
 var GemOrange = function() {
     Gem.call(this, 'images/Gem Orange.png');
-}
+};
 GemOrange.prototype = Object.create(Gem.prototype);
 
 
